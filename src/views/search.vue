36a75214257b27b6cca178da770ffbee7c19e947
Andrew Sunada <asunada@mozilla.com>
2020-01-17 09:46:09 -0800

    adding sort meta to search page

--- src/views/search.vue
+++ src/views/search.vue
@@ -7,6 +7,14 @@
         <p class="meta__content">
           604 results for <span class="content__value">{{ searchValue }}</span>
         </p>
+        <div class="meta__sort">
+          <p class="sort__heading">Sort by:</p>
+          <div class="sort__row">
+            <select class="sort__value">
+              <option>Most Recent</option>
+            </select>
+          </div>
+        </div>
       </aside>
       <section class="search__results">
         <SecondaryVideoDisplay class="results__item" />
@@ -46,8 +54,22 @@ export default {
   margin: 3em auto;
 }
 
+.search__main h1 {
+  margin: 0;
+}
+
+.search__meta {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-end;
+}
+
 .search__meta .meta__content .content__value {
   font-weight: bold;
+}
+
+.search__meta .meta__sort {
 }
 
 .search__results {
