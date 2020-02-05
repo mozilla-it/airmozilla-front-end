af6fc0f1858a22bd2874ee3ff93e1437aa8973f8
Marty Ballard <marty331@gmail.com>
2020-02-04 21:35:52 -0600

    Fixed import names so project will run correctly

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/home.vue";
+import Home from "../views/Home.vue";
 import Player from "../views/player.vue";
 import Search from "../views/search.vue";
 
